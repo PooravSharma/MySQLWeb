c765facdfd463abf1f92f03f0e4f2817b36172d3
Poorav Sharma <57639207+PooravSharma@users.noreply.github.com>
2022-09-21 14:53:17 +0800

    Add files via upload

--- Question1.php
+++ Question1.php
@@ -7,6 +7,7 @@
 <head>
 <title>Question One</title>
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
  <link rel="stylesheet" type="text/css" href="formating.css">
 </head>
 
@@ -14,18 +15,44 @@
 <div id= "Question1">
 
 <!-- List for the Navigaation bar Bar -->
-	<?php
-		include_once('navigation.php');  
-	?>
+<?php
+include_once('navigation.php');  
+try{
+$connection = mysqli_connect("localhost", "adminer", "P@ssw0rd ", "assessmentTwo");
+}
+catch(PDOException $e){
+printf("Connect failed: %s\n", mysqli_connect_error());
+}
 
-<h1>Assessment Two: Fetching Data From A DataBase</h1>
-<br>
-<div class= 'container-fluid'>
-<p>This multi-page website will utilise the Bootstrap, PHP, CSS and HTML to display data from a database. </p>
-<p>Poorav Sharma</p>
-<p>30045900</p>
+$query = 'SELECT * FROM questions WHERE Question = 1';
+<table>
+<tr>
+<th> Question </th>
+<th> Topic </th>
+<th>Description</th>
+<th>Answer</th>
+</tr>
+
+try{	
+
+ while ($row = mysql_fetch_array(query)) {
+?>
+<tr>
+	<td><?php echo $row['Question'];?></td>
+	<td><?php echo $row['Topic'];?></td>
+	<td><?php echo $row['Description'];?></td>
+	<td><?php echo $row['Answer'];?></td>
+</tr>
+<?php
+ }
+ }
+ catch ((PDOException $e){
+ 	echo "Connection Error: " . $e->getMessage();
+ }
+</table>
+$database->disconnect();
+?>
+
 </div>
-
 </body>
-</div>
 </html>
